a93f1469c3a23850cdc00956a7df34d66e455949
wuyu0107 <mkim050107@gmail.com>
2024-08-28 11:57:19 +0700

    Add PWS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-78d%5!wce)dnewvlcn^)5kh2)#g#_0ktak4#9&&yi**tloygcx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "min-kim-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
