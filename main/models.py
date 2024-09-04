0080d28c402cd211b2dc9002816814572f2b342c
wuyu0107 <mkim050107@gmail.com>
2024-09-04 10:29:13 +0700

    Finished tutorial 1

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class MoodEntry(models.Model):
+    mood = models.CharField(max_length=255)
+    time = models.DateField(auto_now_add=True)
+    feelings = models.TextField()
+    mood_intensity = models.IntegerField()
+    sadness_level = models.IntegerField()
+
+    @property
+    def is_mood_strong(self):
+        return self.mood_intensity > 5
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2306199743',
+        'name': 'Min Kim',
+        'class': 'PBP KKI'
+    }
+
+    return render(request, "main.html", context)
